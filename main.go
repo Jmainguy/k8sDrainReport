e9cc078ba1460f9d2426350f056c61488cb57810
Jonathan Mainguy <jon@soh.re>
2019-08-09 10:03:27 -0400

    gofmw -w main.go

--- main.go
+++ main.go
@@ -40,44 +40,44 @@ func main() {
 	}
 
 	for _, v := range pods.Items {
-        if v.Status.Phase == "Running" {
-            i++
-        }
-    }
-    fmt.Printf("Cluster: %s\n", config.Host)
-    fmt.Printf("There are %d running pods in the cluster\n", i)
-    i = 0
-    fmt.Println("========================================")
-    fmt.Println("Pods without owners")
-    fmt.Println("========================================")
+		if v.Status.Phase == "Running" {
+			i++
+		}
+	}
+	fmt.Printf("Cluster: %s\n", config.Host)
+	fmt.Printf("There are %d running pods in the cluster\n", i)
+	i = 0
+	fmt.Println("========================================")
+	fmt.Println("Pods without owners")
+	fmt.Println("========================================")
 	// Loop through all pods
 	for _, v := range pods.Items {
 		// List pods without ownership
 		if len(v.ObjectMeta.OwnerReferences) == 0 {
-            if v.Status.Phase == "Running" {
-                // Ignore kube-system, it has non owned pods, this is normal.
-                if v.ObjectMeta.Namespace != "kube-system" {
-	    		    fmt.Printf("Namespace: %s, Name: %s, NodeName: %s\n", v.ObjectMeta.Namespace, v.ObjectMeta.Name, v.Spec.NodeName)
-    			    i++
-                }
-            }
+			if v.Status.Phase == "Running" {
+				// Ignore kube-system, it has non owned pods, this is normal.
+				if v.ObjectMeta.Namespace != "kube-system" {
+					fmt.Printf("Namespace: %s, Name: %s, NodeName: %s\n", v.ObjectMeta.Namespace, v.ObjectMeta.Name, v.Spec.NodeName)
+					i++
+				}
+			}
 		}
 	}
 
 	fmt.Printf("There are %d pods without ownership\n", i)
 
-    fmt.Println("")
-    fmt.Println("========================================")
-    fmt.Println("Pod Distruption Budget, Potential Issues")
-    fmt.Println("========================================")
-    // Get PodDisruptionBudgets
-    pdbs, err := clientset.PolicyV1beta1().PodDisruptionBudgets("").List(metav1.ListOptions{})
+	fmt.Println("")
+	fmt.Println("========================================")
+	fmt.Println("Pod Distruption Budget, Potential Issues")
+	fmt.Println("========================================")
+	// Get PodDisruptionBudgets
+	pdbs, err := clientset.PolicyV1beta1().PodDisruptionBudgets("").List(metav1.ListOptions{})
 
-    for _, v := range pdbs.Items {
-        if v.Status.PodDisruptionsAllowed == 0 {
-	        fmt.Printf("Namespace: %s, Name: %s, DisruptionsAllowed: %d\n", v.ObjectMeta.Namespace, v.ObjectMeta.Name, v.Status.PodDisruptionsAllowed)
-        }
-    }
+	for _, v := range pdbs.Items {
+		if v.Status.PodDisruptionsAllowed == 0 {
+			fmt.Printf("Namespace: %s, Name: %s, DisruptionsAllowed: %d\n", v.ObjectMeta.Namespace, v.ObjectMeta.Name, v.Status.PodDisruptionsAllowed)
+		}
+	}
 }
 
 func homeDir() string {
